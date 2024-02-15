d0f96efa8e485d8abe850e963719fe67a92a66d6
Андрей Скляр <andreas.skliar@gmail.com>
2024-02-15 12:37:54 +0200

    upd main

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/drink-code-frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
