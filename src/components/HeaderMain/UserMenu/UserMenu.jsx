808baa52883ceef9b02014444096bb27cc06dbcb
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-02-17 21:20:09 +0100

    update

--- src/components/HeaderMain/UserMenu/UserMenu.jsx
+++ src/components/HeaderMain/UserMenu/UserMenu.jsx
@@ -1,12 +1,12 @@
 // @ts-nocheck
-import { avatarImage } from "./UserMenu.styled"
+import { AvatarImage } from "./UserMenu.styled"
 
 
 const UserMenu = () => {
   return (
     <div>
         <div>
-            <avatarImage src="/src/assets/img/default-avatar.png" alt="Avatar by defult" />
+            <AvatarImage src="/src/assets/img/default-avatar.png" alt="Avatar by defult" />
         </div>
         <p>Vladyslav</p>
     </div>
--- src/components/HeaderMain/UserMenu/UserMenu.styled.jsx
+++ src/components/HeaderMain/UserMenu/UserMenu.styled.jsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
 
-export const avatarImage = styled.img`
+export const AvatarImage = styled.img`
     width: 32px;
     height: 32px;
     border-radius: 50%;
+`
+
+export const BtnUserMenu = styled.button`
+    background-color: inherit;
+    border: none;
+    outline: none;
 `
