5d31d9eac89fa8edbc290aa85f3c8fc1c9da9a9e
RomanMatkovskyi <roman.matkovskyii@gmail.com>
2024-02-18 19:33:27 +0100

    404 test push

--- src/pages/ErrorPage/ErrorPage.styled.jsx
+++ src/pages/ErrorPage/ErrorPage.styled.jsx
@@ -1,18 +1,101 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-top: 500px;
+export const ErrorSection = styled.section`
+  background-color: #0a0a11;
+  padding-top: 188px;
+  padding-bottom: 260px;
+  position: relative;
+
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    padding-top: 200px;
+    padding-bottom: 170px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding-top: 200px;
+    padding-bottom: 139px;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 106px;
+    left: -399px;
+    width: 520px;
+    height: 550px;
+    border-radius: 100%;
+    background-color: rgba(64, 112, 205, 0.5);
+    filter: blur(209.708740234375px);
+    @media screen and (min-width: 768px) and (max-width: 1279px) {
+      width: 784px;
+      height: 830px;
+      left: -719px;
+      bottom: -160px;
+    }
+
+    @media screen and (min-width: 1280px) {
+      width: 784px;
+      height: 849px;
+      left: -706px;
+      bottom: -245px;
+    }
+  }
 `;
 
-export const Title = styled.h1`
-  color: #ffffff;
-  text-shadow:
-    0 0 5px #00baff,
-    0 0 10px #00baff,
-    0 0 20px #00baff,
-    0 0 40px #00baff,
-    0 0 80px #00baff;
+export const TitleContainer = styled.div`
+  width: 241px;
+  margin: 0 auto;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    width: 301px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 402px;
+  }
+`;
+
+export const TitleElement = styled.p`
+  font-weight: 600;
+  font-size: 120px;
+  line-height: 102%;
+  color: rgba(243, 243, 243, 0.1);
+
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    font-size: 150px;
+    line-height: 82%;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 200px;
+    line-height: 100%;
+  }
+`;
+export const ErrorImg = styled.span`
+  width: 123px;
+  height: 156px;
+  position: absolute;
+  left: 59px;
+  display: inline-block;
+  background-image: url('/src/pages/ErrorPage/404_img.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    background-image: url('/src/pages/ErrorPage/404_img_tab.png');
+    width: 158px;
+    height: 201px;
+    left: 71.5px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    background-image: url('/src/pages/ErrorPage/404_img_desc.png');
+    width: 158px;
+    height: 201px;
+    left: 122px;
+  }
 `;
