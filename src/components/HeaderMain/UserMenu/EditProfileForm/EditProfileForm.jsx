d8d6d6d68824ed9738d0296a85e23913245474c7
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-02-19 21:27:30 +0100

    update

--- src/components/HeaderMain/UserMenu/EditProfileForm/EditProfileForm.jsx
+++ src/components/HeaderMain/UserMenu/EditProfileForm/EditProfileForm.jsx
@@ -1,36 +1,70 @@
 // @ts-nocheck
+import { useState } from 'react';
 import {
-    Backdrop,
-    BtnClose,
-    ModalWindow,
-    SvgIconClose,
-  } from '../LogoutSure/LogoutSure.styled';
-import { AddAvatar, Avatar } from './EditProfileForm.styled';
+  Backdrop,
+  BtnClose,
+  ModalWindow,
+  SvgIconClose,
+} from '../LogoutSure/LogoutSure.styled';
+import {
+  AddAvatar,
+  Avatar,
+  AvatarContainer,
+  ChangeNameInput,
+  SaveChangeBtn,
+  FormEdit,
+  SvgIcon,
+  EditNameButton,
+  LabelChangeName,
+} from './EditProfileForm.styled';
 
-const EditProfileForm = ({setIsEditOpen}) => {
- const handleClick = () => setIsEditOpen(false)
+const EditProfileForm = ({ setIsEditOpen }) => {
+  const [editName, setEditName] = useState(false);
+  const handleClick = () => setIsEditOpen(false);
   return (
     <Backdrop>
-    <ModalWindow>
-      <div>
-        <BtnClose type="button" onClick={handleClick}>
-          <SvgIconClose>
-            <use xlinkHref="/src/assets/icons/icons.svg#icon-close"></use>
-          </SvgIconClose>
-        </BtnClose>
-      </div>
-      <div>
-        <form>
-            <div>
-                <Avatar src="/src/assets/images/header/default-avatar.png" alt="avatar of the user" width={80} height={80}/>
-                <AddAvatar type="file" name='avatar' id='avatar'/>
-            </div>
-        </form>
-      </div>
-     
-    </ModalWindow>
-  </Backdrop>
-  )
-}
+      <ModalWindow>
+        <div>
+          <BtnClose type="button" onClick={handleClick}>
+            <SvgIconClose>
+              <use xlinkHref="/src/assets/icons/icons.svg#icon-close"></use>
+            </SvgIconClose>
+          </BtnClose>
+        </div>
+        <div>
+          <FormEdit>
+            <AvatarContainer>
+              <Avatar
+                src="/src/assets/images/header/default-avatar.png"
+                alt="avatar of the user"
+                width={80}
+                height={80}
+              />
+              <AddAvatar type="file" name="avatar" id="avatar" />
+            </AvatarContainer>
+            <LabelChangeName htmlFor="name">
+              <ChangeNameInput
+                type="text"
+                name="name"
+                id="name"
+                defaultValue="Vladyslav"
+                readOnly={!editName ? true : false}
+              />
+              <EditNameButton
+                type="button"
+                onClick={() => setEditName((prev) => !prev)}
+              >
+                <SvgIcon>
+                  <use xlinkHref="/src/assets/icons/icons.svg#icon-pencil"></use>
+                </SvgIcon>
+              </EditNameButton>
+            </LabelChangeName>
+            <SaveChangeBtn type="submit">Save changes</SaveChangeBtn>
+          </FormEdit>
+        </div>
+      </ModalWindow>
+    </Backdrop>
+  );
+};
 
-export default EditProfileForm
+export default EditProfileForm;
