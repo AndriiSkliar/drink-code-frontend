70f6bb2285e5b67dff4f40f7c6f5f23943bcd831
AntKhom <antonhomenko1988@gmail.com>
2024-02-19 11:02:37 -0500

    redux

---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,117 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://drink-code-backend.onrender.com/api/';
+const authHeaderToken = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+const signUp = createAsyncThunk(
+  'auth/signup',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/auth/signup', credentials);
+      authHeaderToken.set(data.token); 
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message); 
+    }
+  }
+);
+
+const signIn = createAsyncThunk(
+  'auth/signin',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/auth/signin', credentials);
+      authHeaderToken.set(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+const signOut = createAsyncThunk('auth/signout', async (_, thunkAPI) => {
+  try {
+    await axios.post('/auth/signout');
+    authHeaderToken.unset();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+const currentUser = createAsyncThunk(
+  'auth/currentUser',
+  async (_, thunkAPI) => {
+    const { token } = thunkAPI.getState().auth;
+
+    if (!token) {
+      return thunkAPI.rejectWithValue('Unable to fetch User');
+    }
+
+    authHeaderToken.set(token);
+
+    try {
+      const res = await axios.get('/users/current');
+
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// export const subscribeEmail = createAsyncThunk(
+//   '/auth/subscribe',
+//   async (data, thunkAPI) => {
+//     try {
+//       await axios.post('/users/subscribe', data);
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// const themeThunk = createAsyncThunk(
+//   'auth/theme',
+//   async (payload, { rejectWithValue }) => {
+//     try {
+//       const res = await axios.patch('/users/theme', payload);
+//       return res.data;
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+export const updateUser = createAsyncThunk(
+  'auth/update',
+  async (data, thunkAPI) => {
+    try {
+      const response = await axios.patch('/users/update', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const authOperations = {
+  signUp,
+  signIn,
+  signOut,
+  currentUser,
+//   subscribeEmail,
+//   themeThunk,
+  updateUser,
+};
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,72 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { authOperations } from './authOperations';
+
+const initialState = {
+  user: { name: '', email: '', birthDate: '', avatar: '' },
+  token: '',
+  isLoggedIn: false,
+  isRefreshing: false,
+  isSubscribed: false,
+  theme: 'dark',
+  userAuth: null,
+  userFavorite: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    toggleTheme(state) {
+      state.theme = state.theme === 'light' ? 'dark' : 'light';
+    },
+  },
+  extraReducers: builder =>
+    builder
+      .addCase(authOperations.signUp.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(authOperations.signIn.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+        console.log(state.token);
+      })
+      .addCase(authOperations.signOut.fulfilled, state => {
+        state.user = { name: '', email: '', birthDate: '' };
+        state.token = '';
+        state.isLoggedIn = false;
+      })
+      .addCase(authOperations.signOut.rejected, state => {
+        state.user = { name: '', email: '', birthDate: '' };
+        state.token = '';
+        state.isLoggedIn = false;
+      })
+      .addCase(authOperations.currentUser.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(authOperations.currentUser.fulfilled, (state, { payload }) => {
+        // state.theme = payload.user.theme;
+        state.user = payload.user;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+        state.userAuth = payload.user.userAuth;
+        state.userFavorite = payload.user.userFavorite;
+      })
+      .addCase(authOperations.currentUser.rejected, state => {
+        state.isRefreshing = false;
+      })
+      .addCase(authOperations.subscribeEmail.fulfilled, state => {
+        state.isSubscribed = true;
+      })
+      .addCase(authOperations.updateUser.fulfilled, (state, { payload }) => {
+        state.user.name = payload.name;
+        payload.avatar && (state.user.avatar = payload.avatar);
+      }),
+});
+
+export const authReducer = authSlice.reducer;
+
+export const { toggleTheme } = authSlice.actions;
