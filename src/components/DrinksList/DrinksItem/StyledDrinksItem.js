f453302d4d27f8b1189a5e17d165f35aa4e54565
maershaa <valeria.work2022@gmail.com>
2024-02-23 09:47:32 +0200

    bugfix colorfull wrapper
    обертку радужную чуть подредактировала

--- src/components/DrinksList/DrinksItem/StyledDrinksItem.js
+++ src/components/DrinksList/DrinksItem/StyledDrinksItem.js
@@ -5,23 +5,38 @@ export const StyledDrinksItem = styled.li`
   /* width: 335px;
   height: 360px; */
 
-  /* Стили для подсветки карточек с коктейлями */
-  background: transparent;
-  position: relative;
+  @media (min-width: 768px) {
+    /* width: 342px;
+    height: 360px; */
+    margin-bottom: 80px;
+    width: calc(150% - 20px); /* Две картинки с промежутком в 20px */
+  }
 
-  /* Hover эффект для карточки */
-  &:hover {
+  @media (min-width: 1280px) {
+    /* width: 400px;
+    height: 400px; */
+    width: calc(100% - 40px); /* Три картинки с промежутками в 20px */
+  }
+
+  .imgContainer {
+    margin-bottom: 14px;
+    /* Подсветка карточки с коктейлем */
+    background: transparent;
+    position: relative;
+  }
+
+  .imgContainer:hover {
     border-radius: 8px;
     position: relative;
     background: linear-gradient(0deg, #000, #272727);
-    transition: background cubic-bezier(0.25, 0.1, 0.25, 1) 0.3s; 
-    }
+    transition: background cubic-bezier(0.25, 0.1, 0.25, 1) 0.3s; /* Плавный переход цвета */
+  }
 
   /* Эффект радужной тени */
-  &:hover:before,
-  &:hover:after,
-  &:focus:before,
-  &:focus:after {
+  .imgContainer:hover:before,
+  .imgContainer:hover:after,
+  .imgContainer:focus:before,
+  .imgContainer:focus:after {
     content: '';
     position: absolute;
     left: -2px;
@@ -44,7 +59,10 @@ export const StyledDrinksItem = styled.li`
     height: calc(100% + 4px);
     border-radius: 8px;
     z-index: -1;
-    animation: steam 30s linear infinite; /* продолжительность анимации */
+    animation-name: steam; /* имя анимации, определенной ниже с использованием @keyframes */
+    animation-duration: 16s; /* продолжительность анимации */
+    animation-timing-function: linear; /* анимация идет с постоянной скоростью */
+    animation-iteration-count: infinite; /* бесконечное повторение */
   }
 
   /* Анимация радужной тени */
@@ -61,74 +79,46 @@ export const StyledDrinksItem = styled.li`
   }
 
   /* Эффект размытия */
-  &:after {
+  .imgContainer:after {
     filter: blur(20px);
-    transition: filter cubic-bezier(0.25, 0.1, 0.25, 1) 0.3s;
+    transition: filter cubic-bezier(0.25, 0.1, 0.25, 1) 0.3s; /* Плавный переход размытия */
   }
 
-
-  /* //!! Тут эти стили закончились*/
-
-  @media (min-width: 768px) {
-    /* width: 342px;
-    height: 360px; */
-    margin-bottom: 80px;
-
-    width: calc(150% - 20px); /* Две картинки с промежутком в 20px */
-  }
-
-  @media (min-width: 1280px) {
-    /* width: 400px;
-    height: 400px; */
-    width: calc(100% - 40px); /* Три картинки с промежутками в 20px */
-  }
-
-  @media (min-width: 768px) {
-    /* width: 342px;
-    height: 360px; */
-    margin-bottom: 80px;
-
-    width: calc(150% - 20px); /* Две картинки с промежутком в 20px */
-  }
-
-  @media (min-width: 1280px) {
-    /* width: 400px;
-    height: 400px; */
-    width: calc(100% - 40px); /* Три картинки с промежутками в 20px */
-  }
-
-  .imgContainer {
+  .drinkImgWrapper {
     position: relative;
-    border-radius: 8px;
     overflow: hidden;
-
-    margin-bottom: 14px;
-  }
-
-  /* Добавляем градиент для затемнения на фотки коктейлей */
-  .imgContainer::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    border-radius: 8px;
-    background: linear-gradient(
-      180deg,
-      rgba(10, 10, 17, 0.03) 2.37%,
-      rgba(10, 10, 17, 0.96) 95.31%
-    );
-    z-index: 1;
-  }
-
-  .drinkImg {
     width: 100%;
     height: 100%;
     border-radius: 8px;
     object-fit: cover;
     box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+
+    /* Градиент для затемнения на фото коктейля */
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      border-radius: 8px;
+      background: linear-gradient(
+        180deg,
+        rgba(10, 10, 17, 0.03) 2.37%,
+        rgba(10, 10, 17, 0.96) 95.31%
+      );
+      z-index: 1;
+    }
   }
+
+
+  /* .drinkImg {
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+    object-fit: cover;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  } */
 
   .drinkName {
     font-weight: 500;
