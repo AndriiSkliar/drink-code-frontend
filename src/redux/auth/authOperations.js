95a3f2b636d33c5a8a9198a786c70974d63db528
AntKhom <antonhomenko1988@gmail.com>
2024-02-23 10:10:07 -0500

    return external host

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -3,12 +3,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const instance = axios.create({
-    // baseURL: 'https://drink-code-backend.onrender.com/api',
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://drink-code-backend.onrender.com/api',
+    // baseURL: 'http://localhost:3000/api',
 });
 // для внешнего хостинга
-// axios.defaults.baseURL = 'https://drink-code-backend.onrender.com/api';   
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://drink-code-backend.onrender.com/api';   
+// axios.defaults.baseURL = 'http://localhost:3000/api';
 
 const authHeaderToken = {
   set(token) {
