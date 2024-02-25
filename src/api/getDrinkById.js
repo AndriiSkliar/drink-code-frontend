140286f041a676b2387707a7d8f011ad7a31ff0f
Semeniuk Dmytro <semdimas@gmail.com>
2024-02-25 16:16:03 +0200

    add styles

--- src/api/getDrinkById.js
+++ src/api/getDrinkById.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
+
 const BASE_URL = 'https://drink-code-backend.onrender.com';
-// const BASE_URL = 'http://localhost:3000';
 
 export const getDrinkByID = async (drinkId) => {
   try {
     const token =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZGIxZWJlOTVlMWJiNzg5YzM1ZGQyNyIsImlhdCI6MTcwODg2NjA0OSwiZXhwIjoxNzA4OTQ4ODQ5fQ.osp_1ZqYZdy5i0U9T89EM5FyIwkFMkjxuuIF-rxf3Fs';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZGIzY2ZmODgyMmYyYjAxNmExOTYwOSIsImlhdCI6MTcwODg2NzIwMCwiZXhwIjoxNzA4OTUwMDAwfQ.8k32-dm-kV8qwa9g7uaIda9eVK8c8FICKre8GGZ2ZC0';
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -13,6 +13,7 @@ export const getDrinkByID = async (drinkId) => {
     };
     const resp = await axios.get(`${BASE_URL}/api/drinks/${drinkId}`, config);
     const data = resp.data;
+    console.log('data: ', data);
     return data;
   } catch (error) {
     console.error(error);
--- src/pages/DrinkPage/DrinkPage.jsx
+++ src/pages/DrinkPage/DrinkPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { StyledDrinkPage } from './DrinkPage.styled';
 import { getDrinkByID } from '../../api/getDrinkById';
