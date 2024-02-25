ef15cce753d5476fae4cbb622bee458650bd13f7
KulibabaYuliia <kulibaba120@gmail.com>
2024-02-25 11:41:24 +0200

    fix header logo and styles

--- src/components/HeaderMain/Header.styled.jsx
+++ src/components/HeaderMain/Header.styled.jsx
@@ -7,35 +7,21 @@ export const NavigationHeader = styled.nav`
 `;
 
 export const MainHeader = styled.header`
-  padding-top: 20px;
-  padding-bottom: 20px;
+  padding: 20px 22px;
   border-bottom: 2px solid var(--border-header);
   background-color: transparent;
+
+  @media screen and (min-width: 768px) {
+    padding: 28px 32px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding: 28px 100px;
+  }
 `;
 
 export const MobileBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`;
-
-export const ContainerHeader = styled.div`
-  max-width: 768px;
-  margin: 0 auto;
-  padding: 0 10px;
-
-  @media screen and (min-width: 375px) {
-    padding: 0 20px;
-  }
-
-  @media screen and (min-width: 768px) {
-    padding-left: 32px;
-    padding-right: 32px;
-  }
-
-  @media screen and (min-width: 1280px) {
-    max-width: 1280px;
-    padding-left: 20px;
-    padding-right: 20px;
-  }
 `;
