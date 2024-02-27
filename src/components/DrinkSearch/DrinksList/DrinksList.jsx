c3c0a00779d4a82b0e07ef91875ba1d60df82df2
nikitkachan <nikyc777@gmail.com>
2024-02-27 09:59:39 +0200

    сделал пагинацию на drinks

--- src/components/DrinkSearch/DrinksList/DrinksList.jsx
+++ src/components/DrinkSearch/DrinksList/DrinksList.jsx
@@ -5,41 +5,6 @@ import { StyledDrinksList } from './StyledDrinksList';
 import DrinkList from '../../DrinkList/DrinkList';
 
 const DrinksList = ({ drinks }) => {
-  // Фильтруем коктейли по указанной категории
-  // const filteredDrinks = drinks.filter((drink) => drink.category === title);
-
-  // Хук состояния для отслеживания видимого количества коктейлей в зависимости от ширины экрана
-  const [visibleDrinks, setVisibleDrinks] = useState(1);
-
-  // useEffect для обработки изменения размера экрана и обновления видимого количества коктейлей
-  useEffect(() => {
-    // Функция обработки изменения размера экрана
-    const handleResize = () => {
-      const screenWidth = window.innerWidth;
-
-      // Установка видимого количества коктейлей в зависимости от ширины экрана
-      if (screenWidth >= 1280) {
-        setVisibleDrinks(9);
-      } else if (screenWidth >= 768) {
-        setVisibleDrinks(8);
-      } else {
-        setVisibleDrinks(10);
-      }
-    };
-
-    // Добавляем слушателя события изменения размера экрана и вызываем функцию handleResize при монтировании компонента
-    window.addEventListener('resize', handleResize);
-    handleResize();
-    console.log('drinks', drinks.length);
-    // Очищаем слушателя события при размонтировании компонента, чтобы избежать утечек памяти
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []); // Пустой массив зависимостей гарантирует вызов useEffect только при монтировании и размонтировании
-
-  // Получаем ограниченное количество коктейлей в соответствии с видимым количеством
-  const limitedDrinks = drinks.slice(0, visibleDrinks);
-
   return (
     <DrinkList className="drinksList">
       {limitedDrinks.map((drink) => (
